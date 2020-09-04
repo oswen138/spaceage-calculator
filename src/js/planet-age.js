5c60ac72433f5727edaf1ab9f6d287000a80f518
ophelia swen <ophelia.swen@gmail.com>
2020-09-04 11:54:05 -0700

    add just enough code to create planet age object and call the prototype method in the age js file

--- src/js/planet-age.js
+++ src/js/planet-age.js
@@ -1,9 +1,10 @@
-export class agePlanet {
-  constructor(humanAge, yearsAlive) {
-    this.jupiter.age = jupiter.age;
-  }
-
-age() {
-  if ()
+function planetAge(mercuryAge, venusAge, marsAge, jupiterAge) {
+  this.mercuryAge = mercuryAge;
+  this.venusAge = venusAge;
+  this.marsAge = marsAge;
+  this.jupiterAge = jupiterAge;
 }
+
+planetAge.prototype.checkType = function() {
+  return "approaching takeoff!";
 }
