ef1538e29de93790956b541f66b57a69b6a6ca97
ophelia swen <ophelia.swen@gmail.com>
2020-09-04 12:33:01 -0700

    try to run first test

---
+++ _tests_/age.test.js
@@ -0,0 +1,32 @@
+export default function planetAge from './../src/planet-age.js';
+
+describe('planetAge', function() {
+  let inputAge;
+}
+
+
+//test for calculating age in mercury years
+describe('agePlanet', () => {
+  test('should calculate age on Mercury', () => {
+    const mercuryAge = new mercuryAge(88);
+    expect()
+
+//test for calculating age in venus years
+
+
+//test for calculating age in Mars years
+
+
+//test for calculating age in Jupiter years
+
+
+//test to determine how many years left to live on planet
+
+//test to determine years left on planet based on life expectancy
+
+
+//test to determine life expectancy based on eating chocolate
+
+
+//test to determine life expectancy based on drinking juice
+
