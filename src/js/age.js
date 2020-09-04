4a580bc3b8154fd5edce5a0039d570bc4c9a0457
ophelia swen <ophelia.swen@gmail.com>
2020-09-04 09:55:45 -0700

    read over weeks lesson and figure out how to approach this

--- src/js/age.js
+++
@@ -1,3 +0,0 @@
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
---
+++ src/js/jupiter-age.js
@@ -0,0 +1,9 @@
+export class ageJupiter {
+  constructor(humanAge, yearsAlive) {
+    this.jupiter.age = jupiter.age;
+  }
+
+age() {
+  if ()
+}
+}
